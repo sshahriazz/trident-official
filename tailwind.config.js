b34bd7c6ee4ace3c024ef845074fca7cf8d29106
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-09-23 00:19:38 +0600

    nextjs, tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: "#F4F7FD",
+        primary: "#1585ED",
+        primaryDark: "#0A1133",
+        secondary: "#F9BF3C",
+        text: "#0A1133",
+        textWhite: "#FFFFFF",
+      },
+      fontFamily: {
+        figtree: "Figtree, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
