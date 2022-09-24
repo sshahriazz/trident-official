bb14d3b97960b03d0ec42afa00694f2bd4088395
Md.Saifur Rahman Rasel <76401265+rasel4627@users.noreply.github.com>
2022-09-25 01:59:29 +0600

    Get smarter blog (#17)
    * Get smarter, with our blog ui added with responsive
    
    * Update index.tsx
    
    * get smarter blog responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "components/hero-section";
 import type { NextPage } from "next";
-import GettingStarted from "../components/get-started";
+import GettingStarted from "components/get-started";
+import BlogSection from "components/blog-section";
 
 const Home: NextPage = () => {
   return (
     <div className="space-y-7">
       <HeroSection />
+      <BlogSection />
       <GettingStarted />
     </div>
   );
