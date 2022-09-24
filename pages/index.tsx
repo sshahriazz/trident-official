30ea06bf7b8e99c8280e3d71c99b3f59a3af64bb
mdmurshed <42938038+mdmurshed@users.noreply.github.com>
2022-09-25 00:59:22 +0600

    trident hero middle section added (#15)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
+import HeroSection from "components/hero-section";
 import type { NextPage } from "next";
 import GettingStarted from "../components/get-started";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="space-y-7">
+      <HeroSection />
       <GettingStarted />
-    </>
+    </div>
   );
 };
 
