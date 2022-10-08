4fadd4cde0546b902a8bf3726bc2dfa93ee9bef9
mdmurshed <42938038+mdmurshed@users.noreply.github.com>
2022-10-08 17:30:10 +0600

    speak section added. (#27)
    Co-authored-by: Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ import CareSection from "components/take-care-section";
 import TaskSection from "components/task-section";
 import TrackSection from "components/track-section";
 import BlogSection from "components/blog-section";
-import { NextSeo } from 'next-seo';
+import ClientSpeak from "components/client-speak/index";
+
+import { NextSeo } from "next-seo";
 import ClientQuote from "components/client-quote";
 
 const Home: NextPage = () => {
@@ -17,42 +19,43 @@ const Home: NextPage = () => {
         description="TA team whose experience is as sharp as a trident. If you want to build quality software then trident is the best place. Quality is our best priority"
         canonical="https://www.teamtrident.co/"
         openGraph={{
-          url: 'https://www.url.ie/a',
-          title: 'Open Graph Title',
-          description: 'Open Graph Description',
+          url: "https://www.url.ie/a",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
           images: [
             {
-              url: 'https://www.example.ie/og-image-01.jpg',
+              url: "https://www.example.ie/og-image-01.jpg",
               width: 800,
               height: 600,
-              alt: 'Og Image Alt',
-              type: 'image/jpeg',
+              alt: "Og Image Alt",
+              type: "image/jpeg",
             },
             {
-              url: 'https://www.example.ie/og-image-02.jpg',
+              url: "https://www.example.ie/og-image-02.jpg",
               width: 900,
               height: 800,
-              alt: 'Og Image Alt Second',
-              type: 'image/jpeg',
+              alt: "Og Image Alt Second",
+              type: "image/jpeg",
             },
-            { url: 'https://www.example.ie/og-image-03.jpg' },
-            { url: 'https://www.example.ie/og-image-04.jpg' },
+            { url: "https://www.example.ie/og-image-03.jpg" },
+            { url: "https://www.example.ie/og-image-04.jpg" },
           ],
-          site_name: 'SiteName',
+          site_name: "SiteName",
         }}
         twitter={{
-          handle: '@handle',
-          site: '@site',
-          cardType: 'summary_large_image',
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
         }}
       />
       <div className="space-y-7">
         <HeroSection />
+        <ClientSpeak />
         <CareSection />
         <TaskSection />
         <TrackSection />
         <BlogSection />
-        <ClientQuote/>
+        <ClientQuote />
         <GettingStarted />
       </div>
     </>
